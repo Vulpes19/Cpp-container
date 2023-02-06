dcc50269537be258792664e20b89f36b69d10044
Vulpes19 <baioumyaymann@gmail.com>
2023-02-06 15:22:19 +0100

    push_back is done

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: abaioumy <abaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 11:25:10 by abaioumy          #+#    #+#             */
-/*   Updated: 2023/02/06 14:26:30 by abaioumy         ###   ########.fr       */
+/*   Updated: 2023/02/06 15:21:47 by abaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,29 @@
 
 int	main( void )
 {
-	ft::vector<int> empty;
-	ft::vector<int> vec1(3, 1);
 	ft::vector<char> vec2(3, 'a');
-	// ft::vector<int> empty = vec1;
-	std::cout << empty.empty() << std::endl;
-	empty = vec1;
-	std::cout << empty.at(0) << std::endl;
-	std::cout << vec2.empty() << std::endl;
-	std::vector<int> empty1;
+	std::vector<char> og_vec2(3, 'a');
+	ft::vector<int> empty;
+	std::vector<int> og_empty;
+
+	empty.push_back(1);
+	og_empty.push_back(1);
+	std::cout << empty.back() << " " << empty.capacity() << " " << empty.size() << std::endl;
+	std::cout << og_empty.back() << " " << og_empty.capacity() << " " << og_empty.size() << std::endl;
+
+	vec2.push_back('b');
+	og_vec2.push_back('b');
+	std::cout << vec2.back() << " " << vec2.capacity() << " " << vec2.size() << std::endl;
+	std::cout << og_vec2.back() << " " << og_vec2.capacity() << " " << og_vec2.size() << std::endl;
+	vec2.push_back('c');
+	og_vec2.push_back('c');
+	std::cout << vec2.back() << " " << vec2.capacity() << " " << vec2.size() << std::endl;
+	std::cout << og_vec2.back() << " " << og_vec2.capacity() << " " << og_vec2.size() << std::endl;
+	vec2.push_back('d');
+	og_vec2.push_back('d');
+	std::cout << vec2.back() << " " << vec2.capacity() << " " << vec2.size() << std::endl;
+	std::cout << og_vec2.back() << " " << og_vec2.capacity() << " " << og_vec2.size() << std::endl;
+	
 	system("leaks test_vector");
 	// std::cout << empty[0] << std::endl;
 }
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: abaioumy <abaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 10:42:13 by abaioumy          #+#    #+#             */
-/*   Updated: 2023/02/06 14:26:48 by abaioumy         ###   ########.fr       */
+/*   Updated: 2023/02/06 15:20:17 by abaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,15 @@ namespace ft
 	{
 		public:
 			//default constructor
-			vector( void ) : data(NULL), size(0) {};
+			vector( void ) : data(NULL), _size(0), _capacity(0) {};
 
 			//fill constructor
-			explicit vector( size_type size, const T &val ) : size(size)
+			explicit vector( size_type size, const T &val ) : _size(size), _capacity(size)
 			{
 				Allocator alloc;
 
-				data = alloc.allocate( size );
-				for ( size_type i = 0; i < size; i++ )
+				data = alloc.allocate( _size );
+				for ( size_type i = 0; i < _size; i++ )
 					alloc.construct(data + i, val);
 			};
 
@@ -46,13 +46,14 @@ namespace ft
 			{
 				Allocator alloc;
 
-				for ( size_type i = 0; i < size; i++ )
+				for ( size_type i = 0; i < _size; i++ )
 					alloc.destroy( data + i );
-				alloc.deallocate( data, size );
-				size = source.size;
-				if ( size > 0 )
-					data = alloc.allocate( size );
-				for ( size_type i = 0; i < size; i++ )
+				alloc.deallocate( data, _capacity );
+				_size = source._size;
+				_capacity = source._capacity;
+				if ( _size > 0 )
+					data = alloc.allocate( _capacity );
+				for ( size_type i = 0; i < _size; i++ )
 					alloc.construct( data + i, source.data[i] );
 			};
 
@@ -63,13 +64,14 @@ namespace ft
 
 				if ( this != &source )
 				{
-					for ( size_type i = 0; i < size; i++ )
+					for ( size_type i = 0; i < _size; i++ )
 						alloc.destroy( data + i );
-					alloc.deallocate( data, size );
-					size = source.size;
-					if ( size > 0 )
-						data = alloc.allocate( size );
-					for ( size_type i = 0; i < size; i++ )
+					alloc.deallocate( data, _capacity );
+					_size = source._size;
+					_capacity = source._capacity;
+					if ( _size > 0 )
+						data = alloc.allocate( _capacity );
+					for ( size_type i = 0; i < _size; i++ )
 						alloc.construct( data + i, source.data[i] );
 				}
 				return (*this);
@@ -79,41 +81,83 @@ namespace ft
 			~vector( void )
 			{
 				Allocator alloc;
-				for ( size_type i = 0; i < size; i++ )
+				for ( size_type i = 0; i < _size; i++ )
 					alloc.destroy( data + i );
-				alloc.deallocate( data, size );
-				size = 0;
+				alloc.deallocate( data, _capacity );
+				_size = 0;
 			};
 
 			//[] operator
 			T	&operator[]( size_type index ) const
 			{
-				if ( index >= size )
-					throw(std::out_of_range("error: out of range"));
-				return ( data[index] );
+				return (index >= _size) ? throw(std::out_of_range("error: out of range")) : data[index];
 			};
 
 			//at member function
 			T	&at( size_type position ) const
 			{
-				return (position >= size) ? throw(std::out_of_range("error: out of range")) : data[position];
+				return (position >= _size) ? throw(std::out_of_range("error: out of range")) : data[position];
 			};
 			
 			//back memeber function
 			T	&back( void ) const
 			{
-				return (size > 0) ? data[size - 1] : throw(std::out_of_range("error out of range"));
+				return (_size > 0) ? data[_size - 1] : throw(std::out_of_range("error out of range"));
 			};
 
 			//empty member function
 			bool	empty( void ) const
 			{
-				return (size == 0) ? true : false;
+				return (_size == 0) ? true : false;
 			};
 
-			
+			//resize memeber function
+			// void	resize( size_type newSize, const T &val )
+			// {
+			// 	Allocate alloc;
+
+			// 	if ( newSize == 0 )
+			// 		throw(std::runtime_error("error: size can't be zero"));
+			// 	if ( newSize < size )
+			// 	{
+					
+			// 	}
+			// };
+
+			//capacity member function
+			size_type	capacity( void ) const
+			{
+				return (_capacity);
+			};
+
+			//size member function
+			size_type	size( void ) const
+			{
+				return (_size);
+			};
+
+			//push_back member function
+			void	push_back( const T &val )
+			{
+				if ( _size + 1 > _capacity )
+				{
+					_capacity += 3;
+					T *newData = alloc.allocate(_capacity);
+					for ( size_type i = 0; i < _size; i++ )
+					{
+						alloc.construct(newData + i, data[i]);
+						alloc.destroy(data + i);
+					}
+					alloc.deallocate(data, _size);
+					data = newData;
+				}
+				_size += 1;
+				data[_size - 1] = val;
+			};
 		private:
 			T	*data;
-			size_type	size;
+			size_type	_size;
+			size_type	_capacity;
+			Allocator	alloc;
 	};
 }
