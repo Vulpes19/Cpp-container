386e5304b403c825c9271b47a817536c287a8f1e
Vulpes19 <baioumyaymann@gmail.com>
2023-02-06 14:01:36 +0100

    added copy constructor, copy assignement operator and [] operator

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: abaioumy <abaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 11:25:10 by abaioumy          #+#    #+#             */
-/*   Updated: 2023/02/06 11:51:43 by abaioumy         ###   ########.fr       */
+/*   Updated: 2023/02/06 13:58:58 by abaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ int	main( void )
 {
 	ft::vector<int> empty;
 	ft::vector<int> vec1(3, 1);
+	// ft::vector<int> empty = vec1;
+	empty = vec1;
+	std::cout << empty[0] << std::endl;
 	std::vector<int> empty1;
-
+	system("leaks test_vector");
 	// std::cout << empty[0] << std::endl;
 }
--- vector.hpp
+++ vector.hpp
@@ -6,13 +6,14 @@
 /*   By: abaioumy <abaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 10:42:13 by abaioumy          #+#    #+#             */
-/*   Updated: 2023/02/06 11:59:27 by abaioumy         ###   ########.fr       */
+/*   Updated: 2023/02/06 14:01:10 by abaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <memory>
 #include <cstddef>
+#include <stdexcept>
 
 typedef size_t size_type;
 // typedef Allocator allocator_type;
@@ -24,20 +25,74 @@ namespace ft
 	class vector
 	{
 		public:
+			//default constructor
 			vector( void ) : data(NULL), size(0) {};
-			// explicit vector( const Allocator &allocator );
+
+			explicit vector( const Allocator &allocator );
+
+			//constructor
 			explicit vector( size_type size, const T &val ) : size(size)
 			{
 				Allocator alloc;
 
-				data = alloc.allocate( size + 2 );
+				data = alloc.allocate( size );
 				for ( size_type i = 0; i < size; i++ )
 					alloc.construct(data + i, val);
 			};
-			// vector( const vector &source );
+
+			//copy constructor
+			vector( const vector &source )
+			{
+				Allocator alloc;
+
+				for ( size_type i = 0; i < size; i++ )
+					alloc.destroy( data + i );
+				alloc.deallocate( data, size );
+				size = source.size;
+				if ( size > 0 )
+					data = alloc.allocate( size );
+				for ( size_type i = 0; i < size; i++ )
+					alloc.construct( data + i, source.data[i] );
+			};
+
+			//copy assignement operator
+			vector	&operator= ( const vector &source )
+			{
+				Allocator alloc;
+
+				if ( this != &source )
+				{
+					for ( size_type i = 0; i < size; i++ )
+						alloc.destroy( data + i );
+					alloc.deallocate( data, size );
+					size = source.size;
+					if ( size > 0 )
+						data = alloc.allocate( size );
+					for ( size_type i = 0; i < size; i++ )
+						alloc.construct( data + i, source.data[i] );
+				}
+				return (*this);
+			};
+
+			//destructor
+			~vector( void )
+			{
+				Allocator alloc;
+				for ( size_type i = 0; i < size; i++ )
+					alloc.destroy( data + i );
+				alloc.deallocate( data, size );
+				size = 0;
+			};
+
+			//[] operator
+			T	&operator[]( size_type index ) const
+			{
+				if ( index >= size )
+					throw(std::out_of_range("error: out of range"));
+				return ( data[index] );
+			};
 		private:
 			T	*data;
 			size_type	size;
 	};
 }
-
