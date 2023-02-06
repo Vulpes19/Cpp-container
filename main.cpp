c8f6e323a1492ea3bbfcb7ac230271e1e25008dd
Vulpes19 <baioumyaymann@gmail.com>
2023-02-06 14:28:06 +0100

    at(), back() and empty() are done

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: abaioumy <abaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 11:25:10 by abaioumy          #+#    #+#             */
-/*   Updated: 2023/02/06 13:58:58 by abaioumy         ###   ########.fr       */
+/*   Updated: 2023/02/06 14:26:30 by abaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@ int	main( void )
 {
 	ft::vector<int> empty;
 	ft::vector<int> vec1(3, 1);
+	ft::vector<char> vec2(3, 'a');
 	// ft::vector<int> empty = vec1;
+	std::cout << empty.empty() << std::endl;
 	empty = vec1;
-	std::cout << empty[0] << std::endl;
+	std::cout << empty.at(0) << std::endl;
+	std::cout << vec2.empty() << std::endl;
 	std::vector<int> empty1;
 	system("leaks test_vector");
 	// std::cout << empty[0] << std::endl;
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: abaioumy <abaioumy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 10:42:13 by abaioumy          #+#    #+#             */
-/*   Updated: 2023/02/06 14:01:10 by abaioumy         ###   ########.fr       */
+/*   Updated: 2023/02/06 14:26:48 by abaioumy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,7 @@ namespace ft
 			//default constructor
 			vector( void ) : data(NULL), size(0) {};
 
-			explicit vector( const Allocator &allocator );
-
-			//constructor
+			//fill constructor
 			explicit vector( size_type size, const T &val ) : size(size)
 			{
 				Allocator alloc;
@@ -39,6 +37,9 @@ namespace ft
 				for ( size_type i = 0; i < size; i++ )
 					alloc.construct(data + i, val);
 			};
+
+			//range constructor
+			// vector( Iterator first, Iterator last );
 
 			//copy constructor
 			vector( const vector &source )
@@ -91,6 +92,26 @@ namespace ft
 					throw(std::out_of_range("error: out of range"));
 				return ( data[index] );
 			};
+
+			//at member function
+			T	&at( size_type position ) const
+			{
+				return (position >= size) ? throw(std::out_of_range("error: out of range")) : data[position];
+			};
+			
+			//back memeber function
+			T	&back( void ) const
+			{
+				return (size > 0) ? data[size - 1] : throw(std::out_of_range("error out of range"));
+			};
+
+			//empty member function
+			bool	empty( void ) const
+			{
+				return (size == 0) ? true : false;
+			};
+
+			
 		private:
 			T	*data;
 			size_type	size;
